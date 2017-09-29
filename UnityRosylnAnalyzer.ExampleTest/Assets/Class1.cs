1dd0b01d9b0ba0d4cc131026dc8061121011d6ed
JP314 <jan.grau95@gmail.com>
2017-09-29 23:05:38 +0200

    Added example gif

---
+++ UnityRosylnAnalyzer.ExampleTest/Assets/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class SampleBehaviour : MonoBehaviour
+{
+
+	private void Update()
+	{
+		var blink = Time.time % 2 < 1;
+		var color = blink ? Color.red : Color.green;
+		GetComponent<SpriteRenderer>().color = color;
+	}
+
+}
